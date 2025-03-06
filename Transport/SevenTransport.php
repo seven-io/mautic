c7aa53b36defbb047184cb59c35c18f3b307f5ad
André Matthies <matthiez@gmail.com>
2025-03-06 13:47:07 +0100

    remove seven client dependency

--- Transport/SevenTransport.php
+++ Transport/SevenTransport.php
@@ -3,9 +3,6 @@
 namespace MauticPlugin\MauticSevenBundle\Transport;
 
 use Exception;
-use libphonenumber\NumberParseException;
-use libphonenumber\PhoneNumberFormat;
-use libphonenumber\PhoneNumberUtil;
 use Mautic\IntegrationsBundle\Exception\IntegrationNotFoundException;
 use Mautic\IntegrationsBundle\Exception\PluginNotConfiguredException;
 use Mautic\IntegrationsBundle\Helper\IntegrationsHelper;
@@ -15,17 +12,12 @@ use Mautic\SmsBundle\Sms\TransportInterface;
 use MauticPlugin\MauticSevenBundle\Exception\SevenPluginException;
 use MauticPlugin\MauticSevenBundle\Integration\SevenIntegration;
 use Monolog\Logger;
-use Seven\Api\Client;
-use Seven\Api\Exception\InvalidOptionalArgumentException;
-use Seven\Api\Exception\InvalidRequiredArgumentException;
-use Seven\Api\Params\SmsParams;
-use Seven\Api\Resource\SmsResource;
 
 /**
  * Class SevenTransport is the transport service for mautic.
  */
 class SevenTransport implements TransportInterface {
-    private ?Client $client = null;
+    private ?string $apiKey = null;
     private bool $clientConfigured = false;
 
     public function __construct(
@@ -39,10 +31,10 @@ class SevenTransport implements TransportInterface {
      * @param Lead $lead
      * @param string $content
      * @return bool|PluginNotConfiguredException|string
-     * @throws InvalidOptionalArgumentException
-     * @throws InvalidRequiredArgumentException
      * @throws SevenPluginException
      * @throws IntegrationNotFoundException
+     * @throws Exception
+     * @noinspection PhpUnused
      */
     public function sendSms(Lead $lead, $content) {
         $this->logger->info('sms via seven', ['content' => $content]);
@@ -50,29 +42,27 @@ class SevenTransport implements TransportInterface {
         $number = $lead->getLeadPhoneNumber();
         if (empty($number)) return false;
 
-        //if (!$this->client) throw new SevenPluginException('There is no client available');
-
         if (!$this->clientConfigured && !$this->configureClient())
             return new PluginNotConfiguredException;
 
-        $util = PhoneNumberUtil::getInstance();
-        $params = new SmsParams($content);
-
         try {
-            $parsed = $util->parse($number, 'US');
-            $number = $util->format($parsed, PhoneNumberFormat::E164);
-            $number = substr($number, 1);
-            $params->addTo($number);
+            $params = [
+                'to' => $number,
+                'text' => $content
+            ];
 
-            $resource = new SmsResource($this->client);
-            $resource->dispatch($params);
-        } catch (NumberParseException $exception) {
-            $this->logger->info('Invalid number format', ['error' => $exception->getMessage()]);
-
-            return 'mautic.seven.failed.invalid_phone_number';
-        } catch (
-        InvalidOptionalArgumentException|InvalidRequiredArgumentException $e
-        ) {
+            $ch = curl_init('https://gateway.seven.io/api/sms');
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                'Accept: application/json',
+                'Content-type: application/json',
+                'SentWith: Mautic',
+                'X-Api-Key: ' . $this->apiKey
+            ]);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_exec($ch);
+            curl_close($ch);
+        } catch (Exception $e) {
             $this->logger->error('seven plugin unhandled exception',
                 ['error' => $e->getMessage(), 'number' => $number]
             );
@@ -108,7 +98,7 @@ class SevenTransport implements TransportInterface {
             $keys = $integrationConfiguration->getApiKeys();
 
             if (isset($keys['apiKey'])) {
-                $this->client = new Client($keys['apiKey'], 'Mautic');
+                $this->apiKey = $keys['apiKey'];
                 $this->clientConfigured = true;
                 return true;
             }
